3acc525a94d55fceeea03a6c31f3a492c9cf5312
EC2 Default User <ec2-user@ip-172-31-47-250.us-west-2.compute.internal>
2023-11-01 19:49:18 +0000

    Initial commit

---
+++ test/paco-cdk-connect.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PacoCdkConnect from '../lib/paco-cdk-connect-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/paco-cdk-connect-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PacoCdkConnect.PacoCdkConnectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
