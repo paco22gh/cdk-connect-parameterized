3e9552f9243f2c27cb88c88f8e097295cd45b4ef
EC2 Default User <ec2-user@ip-172-31-47-250.us-west-2.compute.internal>
2023-11-04 01:43:53 +0000

    first working verison

--- bin/paco-cdk-connect.ts
+++ bin/paco-cdk-connect.ts
@@ -3,6 +3,7 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { PacoCdkConnectStack } from '../lib/paco-cdk-connect-stack';
 
+
 const app = new cdk.App();
 new PacoCdkConnectStack(app, 'PacoCdkConnectStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
@@ -15,7 +16,16 @@ new PacoCdkConnectStack(app, 'PacoCdkConnectStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
+  instanceArn:'arn:aws:connect:us-west-2:122372919864:instance/25e8510b-3248-4d78-bc31-ba21cb0c468f',
+  timezone: cdk.TimeZone.AMERICA_MEXICO_CITY.timezoneName,
+  env: { account: '122372919864', region: 'us-west-2' },
+  name: 'Pacos availability',    description: 'The time to reach out Paco',
+  label_key: 'origin',
+  label_value: 'cdk',
+  sthours: 8,
+  stminutes: 0,
+  ethours: 17,
+  etminutes: 0,
+  daysofweek: [['MONDAY',8,0,17,0],['TUESDAY',8,0,17,0],['WEDNESDAY',8,0,17,0],['THURSDAY',8,0,17,0],['FRIDAY',8,0,14,0]]
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/paco-cdk-connect-stack.ts
+++ lib/paco-cdk-connect-stack.ts
@@ -1,16 +1,92 @@
 import * as cdk from 'aws-cdk-lib';
+import * as connect from 'aws-cdk-lib/aws-connect';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+//import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+
+interface PacoCdkConnectProps extends cdk.StackProps {
+    env: { account: '122372919864', region: 'us-west-2' },
+    instanceArn:string,
+    timezone: string,
+    name: string,
+    description: string,
+    label_key: string,
+    label_value: string,
+    sthours: number,
+    stminutes: number,
+    ethours: number,
+    etminutes: number,
+    daysofweek: any[]
+}
+
+
 
 export class PacoCdkConnectStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: PacoCdkConnectProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    /*const hoursOfOperationSTTimeSliceProperty: connect.CfnHoursOfOperation.HoursOfOperationTimeSliceProperty = {
+      hours: props.sthours,
+      minutes: props.stminutes,
+    };*/
+    
+    /*const hoursOfOperationETTimeSliceProperty: connect.CfnHoursOfOperation.HoursOfOperationTimeSliceProperty = {
+      hours: props.ethours,
+      minutes: props.etminutes,
+    };
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'PacoCdkConnectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const hoursOfOperationConfigPropertym: connect.CfnHoursOfOperation.HoursOfOperationConfigProperty = {
+      day: 'MONDAY',
+      endTime: hoursOfOperationETTimeSliceProperty,
+      startTime: hoursOfOperationSTTimeSliceProperty,
+    };
+    const hoursOfOperationConfigPropertyt: connect.CfnHoursOfOperation.HoursOfOperationConfigProperty = {
+      day: 'THURSDAY',
+      endTime: hoursOfOperationETTimeSliceProperty,
+      startTime: hoursOfOperationSTTimeSliceProperty,
+    };*/
+    //Array to store the list of days and times
+    const hoursOfOperationConfigProperties: any[] = []
+    props.daysofweek.forEach(function (dayarr) {
+      console.log(dayarr[0]);
+      console.log(dayarr[1]);
+      //Interface to store time
+      const hoursOfOperationSTTimeSliceProperty: connect.CfnHoursOfOperation.HoursOfOperationTimeSliceProperty = {
+        hours: dayarr[1],
+        minutes: dayarr[2],
+        };
+      
+      const hoursOfOperationETTimeSliceProperty: connect.CfnHoursOfOperation.HoursOfOperationTimeSliceProperty = {
+        hours: dayarr[3],
+        minutes: dayarr[4],
+        };
+      //Interface to store day
+      const hoursOfOperationConfigProperty: connect.CfnHoursOfOperation.HoursOfOperationConfigProperty = {
+        day: dayarr[0],
+        endTime: hoursOfOperationETTimeSliceProperty,
+        startTime: hoursOfOperationSTTimeSliceProperty,
+       };
+      hoursOfOperationConfigProperties.push(hoursOfOperationConfigProperty)
+    });
+
+    //const hoursOfOperationConfigProperties: any[] = []
+     //hoursOfOperationConfigProperties.push(hoursOfOperationConfigPropertym)
+     //hoursOfOperationConfigProperties.push(hoursOfOperationConfigPropertyt)
+
+//leer props con foreach y usar una misma variable cons para ir agregando al push
+
+    const cfnHoursOfOperation = new connect.CfnHoursOfOperation(this, 'pacoCdkHoursOfOperation', {
+      config: hoursOfOperationConfigProperties,
+      instanceArn: props.instanceArn,
+      name: props.name,
+      timeZone: props.timezone,
+      // the properties below are optional
+      description: props.description,
+      tags: [{
+        key: props.label_key,
+        value: props.label_value,
+      }],
+    });
+
   }
+  
 }
